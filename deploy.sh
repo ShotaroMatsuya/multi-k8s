f1786bb8f82c70269a6a5ef085fb65d04a11486a
ShotaroMatsuya <matsuya@h01.itscom.net>
2021-08-12 23:13:24 +0900

    modified image name

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
-docker build -t shotaromatsuya/multi-client:latest -t shotaromatsuya/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t shotaromatsuya/multi-server:latest -t shotaromatsuya/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t shotaromatsuya/multi-worker:latest -t shotaromatsuya/multi-worker:$SHA -f ./worker/Dockerfile ./worker
-dokcer push shotaromatsuya/multi-client:latest
-dokcer push shotaromatsuya/multi-server:latest
-dokcer push shotaromatsuya/multi-worker:latest
-dokcer push shotaromatsuya/multi-client:$SHA
-dokcer push shotaromatsuya/multi-server:$SHA
-dokcer push shotaromatsuya/multi-worker:$SHA
+docker build -t shotaromatsuya/multi-client-k8s:latest -t shotaromatsuya/multi-client-k8s:$SHA -f ./client/Dockerfile ./client
+docker build -t shotaromatsuya/multi-server-k8s:latest -t shotaromatsuya/multi-server-k8s:$SHA -f ./server/Dockerfile ./server
+docker build -t shotaromatsuya/multi-worker-k8s:latest -t shotaromatsuya/multi-worker-k8s:$SHA -f ./worker/Dockerfile ./worker
+dokcer push shotaromatsuya/multi-clien-k8st:latest
+dokcer push shotaromatsuya/multi-server-k8s:latest
+dokcer push shotaromatsuya/multi-worker-k8s:latest
+dokcer push shotaromatsuya/multi-client-k8s:$SHA
+dokcer push shotaromatsuya/multi-server-k8s:$SHA
+dokcer push shotaromatsuya/multi-worker-k8s:$SHA
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=shotaromatsuya/multi-server:$SHA
-kubectl set image deployments/client-deployment client=shotaromatsuya/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=shotaromatsuya/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=shotaromatsuya/multi-server-k8s:$SHA
+kubectl set image deployments/client-deployment client=shotaromatsuya/multi-client-k8s:$SHA
+kubectl set image deployments/worker-deployment worker=shotaromatsuya/multi-worker-k8s:$SHA
